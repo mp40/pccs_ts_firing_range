df0ca6a04d0d98d8ce1f3b7c710f372a93d58c8a
Matthew Philps <philps.matthew@gmail.com>
2019-11-03 12:20:56 +0900

    basic setup

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        PCCS Firing Range
+      </header>
+      <div className="App-body">
+        Content
+      </div>
+    </div>
+  );
+}
+
+export default App;
