c4a3c6826f22c395bd12836783bb64fad31beb71
Matthew Philps <philps.matthew@gmail.com>
2019-11-05 10:43:28 +0900

    enzyme wiring test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 const App: React.FC = () => {
+  const [text, updateText] = useState('text')
+
   return (
     <div className="App">
       <header className="App-header">PCCS Firing Range</header>
-      <div className="App-body">Content</div>
+      <div className="App-body">
+        <div>Content</div>
+        <button type="button" onClick={():void => updateText('TEST!')}>{text}</button>
+        </div>
     </div>
   );
 };
