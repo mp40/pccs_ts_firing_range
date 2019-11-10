f00842ca4f1aa04cd4d4d5bd1ec8bca4981988ad
Matthew Philps <philps.matthew@gmail.com>
2019-11-11 07:25:24 +0900

    refactored situation

--- src/Situation/data.ts
+++ src/Situation/data.ts
@@ -1,8 +1,15 @@
-const situationModifiers: { [key: string]: number } = {
-  hipFire: -6,
-  rifleOneHand: -7,
-  pistolOneHand: -4,
-  foldedStock: -4
+interface SituationShape {
+  mod: number;
+  heading: string;
+}
+
+const situationModifiers: { [key: string]: SituationShape } = {
+  hipFire: { mod: -6, heading: 'Hip Fire' },
+  rifleOneHand: { mod: -7, heading: 'Firing Rifle One Handed' },
+  pistolOneHand: { mod: -4, heading: 'Firing Pistol One Handed' },
+  foldedStock: { mod: -4, heading: 'Folding Stock Not Used' },
+  bipodNotBraced: { mod: -2, heading: 'Bipod Not Braced' },
+  bipodMount: { mod: 3, heading: 'Bipod Mounted Weapon' }
 };
 
 export default situationModifiers;
--- src/Situation/index.tsx
+++ src/Situation/index.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import situationModifiers from './data';
+
 const Situation: React.FC = () => {
   const [hipFire, toggleHipFire] = useState(false);
   const [rifleOneHand, toggleRifleOneHand] = useState(false);
@@ -8,58 +10,33 @@ const Situation: React.FC = () => {
   const [bipodNotBraced, toggleBipodNotBraced] = useState(false);
   const [bipodMount, toggleBipodMount] = useState(false);
 
+  const renderLabel = (
+    name: string,
+    stateValue: boolean,
+    toggleValue: Function
+  ): JSX.Element => {
+    return (
+      <label className={name}>
+        <span>{situationModifiers[name].heading}</span>
+        <input
+          type="checkbox"
+          checked={stateValue}
+          onChange={(): void => toggleValue(!stateValue)}
+        />
+      </label>
+    );
+  };
+
   return (
     <div className="situationContainer">
       <form className="situationSelectForm">
         <div className="situationToggle">
-          <label className="hipFire">
-            <span>Hip Fire</span>
-            <input
-              type="checkbox"
-              checked={hipFire}
-              onChange={(): void => toggleHipFire(!hipFire)}
-            />
-          </label>
-          <label className="rifleOneHand">
-            <span>Firing Rifle One Handed</span>
-            <input
-              type="checkbox"
-              checked={rifleOneHand}
-              onChange={(): void => toggleRifleOneHand(!rifleOneHand)}
-            />
-          </label>
-          <label className="pistolOneHand">
-            <span>Firing Pistol One Handed</span>
-            <input
-              type="checkbox"
-              checked={pistolOneHand}
-              onChange={(): void => togglePistolOneHand(!pistolOneHand)}
-            />
-          </label>
-          <label className="foldedStock">
-            <span>Folding Stock Not Used</span>
-            <input
-              type="checkbox"
-              checked={foldedStock}
-              onChange={(): void => toggleFoldedStock(!foldedStock)}
-            />
-          </label>
-          <label className="bipodNotBraced">
-            <span>Bipod Not Braced</span>
-            <input
-              type="checkbox"
-              checked={bipodNotBraced}
-              onChange={(): void => toggleBipodNotBraced(!bipodNotBraced)}
-            />
-          </label>
-          <label className="bipodMount">
-            <span>Bipod Mounted Weapon</span>
-            <input
-              type="checkbox"
-              checked={bipodMount}
-              onChange={(): void => toggleBipodMount(!bipodMount)}
-            />
-          </label>
+          {renderLabel('hipFire', hipFire, toggleHipFire)}
+          {renderLabel('rifleOneHand', rifleOneHand, toggleRifleOneHand)}
+          {renderLabel('pistolOneHand', pistolOneHand, togglePistolOneHand)}
+          {renderLabel('foldedStock', foldedStock, toggleFoldedStock)}
+          {renderLabel('bipodNotBraced', bipodNotBraced, toggleBipodNotBraced)}
+          {renderLabel('bipodMount', bipodMount, toggleBipodMount)}
         </div>
       </form>
     </div>
