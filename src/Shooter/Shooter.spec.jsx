4e8b32095ad7aaa34f7e5432a2671353b75f7631
Matthew Philps <philps.matthew@gmail.com>
2019-11-14 21:31:59 +0900

    changed implementation of Shooter

--- src/Shooter/Shooter.spec.jsx
+++ src/Shooter/Shooter.spec.jsx
@@ -4,28 +4,41 @@ import { shallow } from 'enzyme';
 import Shooter from './index';
 
 describe('Shooter', () => {
-  const wrapper = shallow(<Shooter />);
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<Shooter />);
+  });
   it('should start displaying M1 Carbine as default weapon', () => {
     expect(wrapper.text()).toContain('M1 Carbine');
   });
   it('should start with default shooting level of 0', () => {
-    expect(wrapper.find('.selectLevel').props().value).toBe(0);
+    expect(wrapper.text()).toContain('Shooter Level: 0');
   });
   it('should show the weapons aim time mods', () => {
     expect(wrapper.find('.aimTime').text()).toContain('1-21');
     expect(wrapper.find('.aimTime').text()).toContain('110');
   });
   it('should be possible to change shooting level', () => {
-    wrapper.find('.selectLevel').simulate('change', { target: { value: 4 } });
-    expect(wrapper.find('.selectLevel').props().value).toBe(4);
+    wrapper.find('.levelButton').simulate('click');
+    wrapper
+      .find('.levelBtns')
+      .at(4)
+      .simulate('click');
+    expect(wrapper.text()).toContain('Shooter Level: 4');
   });
   it('should update aim time mod when level is changed', () => {
+    wrapper.find('.levelButton').simulate('click');
+    wrapper
+      .find('.levelBtns')
+      .at(4)
+      .simulate('click');
     expect(wrapper.find('.aimTime').text()).toContain('1-11');
     expect(wrapper.find('.aimTime').text()).toContain('1110');
   });
   it('should be possible to select an aim time', () => {
+    wrapper.find('.aimButton').simulate('click');
     wrapper
-      .find('.aimTimeRow')
+      .find('.aimBtns')
       .at(2)
       .simulate('click');
     expect(
@@ -36,11 +49,26 @@ describe('Shooter', () => {
     ).toBe(true);
   });
   it('should display the weapon recoil recovery', () => {
-    wrapper.find('.selectLevel').simulate('change', { target: { value: 0 } });
     expect(wrapper.text()).toContain('Recoil Recovery: 2');
   });
   it('should update recoil recovery on level change', () => {
-    wrapper.find('.selectLevel').simulate('change', { target: { value: 4 } });
+    wrapper.find('.levelButton').simulate('click');
+    wrapper
+      .find('.levelBtns')
+      .at(4)
+      .simulate('click');
     expect(wrapper.text()).toContain('Recoil Recovery: 1');
+  });
+  it('should not be possible to toggle aim selection on when level selection is toggled', () => {
+    wrapper.find('.levelButton').simulate('click');
+    wrapper.find('.aimButton').simulate('click');
+    expect(wrapper.find('.levelBtns').length).toBe(6);
+    expect(wrapper.find('.aimBtns').length).toBe(0);
+  });
+  it('should not be possible to toggle level selection on when aim selection is toggled', () => {
+    wrapper.find('.aimButton').simulate('click');
+    wrapper.find('.levelButton').simulate('click');
+    expect(wrapper.find('.aimBtns').length).toBe(11);
+    expect(wrapper.find('.levelBtns').length).toBe(0);
   });
 });
--- src/Shooter/index.tsx
+++ src/Shooter/index.tsx
@@ -6,7 +6,43 @@ import './Shooter.css';
 
 const Shooter: React.FC = () => {
   const [level, setLevel] = useState(0);
-  const [aims, selectAims] = useState(0);
+  const [aims, selectAims] = useState(1);
+  const [showLevels, toggleShowLevels] = useState(false);
+  const [showAims, toggleShowAims] = useState(false);
+
+  const handleSetLevel = (lvl: number): void => {
+    setLevel(lvl);
+    toggleShowLevels(false);
+  };
+
+  const handleSetAim = (aimCount: number): void => {
+    selectAims(aimCount);
+    toggleShowAims(false);
+  };
+
+  const handleToggleLevels = (): void => {
+    if (showAims) {
+      return;
+    }
+    toggleShowLevels(!showLevels);
+  };
+
+  const handleToggleAims = (): void => {
+    if (showLevels) {
+      return;
+    }
+    toggleShowAims(!showAims);
+  };
+
+  const getClassName = (button: string): string => {
+    if (button === 'level' && showAims) {
+      return 'shooterButton disabled';
+    }
+    if (button === 'aim' && showLevels) {
+      return 'shooterButton disabled';
+    }
+    return 'shooterButton';
+  };
 
   const getAimTimeMod = (index: number): number => {
     return m1Carbine.aim.mod[index] + salMod[level];
@@ -22,11 +58,7 @@ const Shooter: React.FC = () => {
         <div>Select Aim Time</div>
         {m1Carbine.aim.ac.map((aim, index) => {
           return (
-            <div
-              className={`aimTimeRow${applySelected(aim)}`}
-              key={aim}
-              onClick={(): void => selectAims(aim)}
-            >
+            <div className={`aimTimeRow${applySelected(aim)}`} key={aim}>
               <span>{aim}</span>
               <span>{getAimTimeMod(index)}</span>
             </div>
@@ -36,44 +68,64 @@ const Shooter: React.FC = () => {
     );
   };
 
-  const renderShooterLevel = (): JSX.Element => {
-    return (
-      <div>
-        <form>
-          <label>
-            Shooter Level:
-            <select
-              className="selectLevel"
-              value={level}
-              onChange={(event): void =>
-                setLevel(parseInt(event.target.value, 10))
-              }
-            >
-              {[0, 1, 2, 3, 4, 5].map(lvl => {
-                return (
-                  <option key={lvl} value={lvl}>
-                    {lvl}
-                  </option>
-                );
-              })}
-            </select>
-          </label>
-        </form>
-      </div>
-    );
-  };
-
   return (
     <div className="shooterContainer">
-      <div className="shooterDetails">
-        <div>{`Weapon: ${m1Carbine.name}`}</div>
-        <div>{`Recoil Recovery: ${getRecoilRecoveryValue(
-          m1Carbine.kd,
-          level
-        )}`}</div>
-        {renderShooterLevel()}
+      <div className="shooterDetailsContainer">
+        <div className="shooterDetails">
+          <div>{`Weapon: ${m1Carbine.name}`}</div>
+          <div>{`Recoil Recovery: ${getRecoilRecoveryValue(
+            m1Carbine.kd,
+            level
+          )}`}</div>
+          <div>{`Shooter Level: ${level}`}</div>
+          <button
+            type="button"
+            className={`${getClassName('level')} levelButton`}
+            onClick={(): void => {
+              handleToggleLevels();
+            }}
+          >
+            Select Level
+          </button>
+          <button
+            type="button"
+            className={`${getClassName('aim')} aimButton`}
+            onClick={(): void => {
+              handleToggleAims();
+            }}
+          >
+            Select Aims
+          </button>
+        </div>
+        {renderAimTime()}
       </div>
-      {renderAimTime()}
+
+      <div className="selectPanel">
+        {showLevels &&
+          [0, 1, 2, 3, 4, 5].map(lvl => {
+            return (
+              <button
+                key={lvl}
+                className="levelBtns"
+                onClick={(): void => handleSetLevel(lvl)}
+              >
+                {lvl}
+              </button>
+            );
+          })}
+        {showAims &&
+          m1Carbine.aim.ac.map(aimCount => {
+            return (
+              <button
+                key={aimCount}
+                className="aimBtns"
+                onClick={(): void => handleSetAim(aimCount)}
+              >
+                {aimCount}
+              </button>
+            );
+          })}
+      </div>
     </div>
   );
 };
